8a3dfc8c1dd157c0eb116ff288b12c0ea76fe182
ezeitouni <cezeitouni@crimson.ua.edu>
2015-02-06 08:50:08 -0600

    Registration Page Controls and Comments

--- js/login.js
+++ js/login.js
@@ -1,7 +1,9 @@
 // JavaScript Document
-var GPA = 5.0;
 
+//Login Page Controls
 $(document).on('pageinit','#login', function(){
+	
+	//AJAX Call for login
 	$(document).on('click','#loginSubmitBtn', function(){
 		$.ajax({
 			url: 'http://slalomtest2.azurewebsites.net/api/Login/',
@@ -31,11 +33,43 @@ $(document).on('pageinit','#login', function(){
 	});
 });
 
+//Forgot Password Page Controls
 $(document).on('pageinit','#forgotPassword', function(){
+	
+	//AJAX call for password recovery
 	$(document).on('click','#forgotPasswordBtn', function(){
 		$.ajax({
 			url: 'http://slalomtest2.azurewebsites.net/api/forgotpassword/',
 			data: $('#forgotPasswordForm').serialize(),
+			type: 'POST',
+			async: true,
+			contentType:"application/json",
+			dataType:"json",
+			beforeSend: function(){
+				$.mobile.showPageLoadingMsg(true);
+			},
+			complete: function(){
+				$.mobile.hidePageLoadingMsg();
+			},
+			success: function(result){
+				$.mobile.changePage("#login");
+			},
+			error: function(request, error){
+				alert('Sorry, something went wrong.');
+				$.mobile.changePage("#login");
+			}
+		});
+	});
+});
+
+//Registration page Controls
+$(document).on('pageinit','#regisPage', function(){
+	
+	//AJAX call for password recovery
+	$(document).on('click','#submitNewUser', function(){
+		$.ajax({
+			url: 'http://slalomtest2.azurewebsites.net/api/account/register/',
+			data: $('#regisPageForm').serialize(),
 			type: 'POST',
 			async: true,
 			contentType:"application/json",
