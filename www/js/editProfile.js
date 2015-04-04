616423e3ac09ebf2b4526f8990c8fe36c7a6980c
ezeitouni <cezeitouni@crimson.ua.edu>
2015-04-04 14:40:01 -0500

    fixing shit

--- www/js/editProfile.js
+++ www/js/editProfile.js
@@ -1,6 +1,6 @@
 // Input information into edit profile 
 $(document).on('pageshow', '#editProfile', function () {
-$.ajax({
+    $.ajax({
         type: "GET",
         url: S_ROOT + 'api/getprofile',
         async: true,
@@ -19,26 +19,23 @@ $.ajax({
             //Hide loader
         },
         success: function (data) {
-			
-			
-			var picture = GetPic();
-			
-			var profPic = document.getElementById('#regisPortrait1')
-			profPic.src = picture;
-	
-			
-           
-		   /*$("#regisPic1").val(profPic);*/
-		   $("#fname1").val(data.fname);
-		   $("#lname1").val(data.lname);
-		   $("#regisEmail1").val(data.email);
-		   $("#phone1").val(data.phone);
-		   $("#guestemail1").val(data.guestemail);
+
+
+            GetPic();
+
+
+
+            /*$("#regisPic1").val(profPic);*/
+            $("#fname1").val(data.fname);
+            $("#lname1").val(data.lname);
+            $("#regisEmail1").val(data.email);
+            $("#phone1").val(data.phone);
+            $("#guestemail1").val(data.guestemail);
             //Log success
             console.log("profile received");
-           
-		   
-			
+
+
+
         },
         error: function (request, error) {
 
@@ -48,40 +45,35 @@ $.ajax({
         }
 
     });
-function GetPic() {
-	var pic;
-$.ajax({
-        type: "GET",
-        url: S_ROOT + 'api/getpic',
-        async: false,
-        dataType: "json",
-        beforeSend: function (request) {
+    function GetPic() {
+        $.ajax({
+            type: "GET",
+            url: S_ROOT + 'api/getpic',
+            async: true,
+            dataType: "text",
+            beforeSend: function (request) {
 
-            //Attaches credentials to AJAX call
-            request.withCredentials = true;
-            request.setRequestHeader("Authorization", "Bearer " + S_TOKEN);
+                //Attaches credentials to AJAX call
+                request.withCredentials = true;
+                request.setRequestHeader("Authorization", "Bearer " + S_TOKEN);
 
-        },
-        complete: function () {
+            },
+            success: function (data) {
+
+                $("regisPortrait1").attr('src', data);
+                console.log(data);
+            },
+            error: function (request, error) {
+
+                //Log failure
+                var myError = "Error " + request.status + ": " + request.responseJSON.Message;
+                navigator.notification.alert(myError, console.log(myError), "Get Status Failed");
+            }
+
+        });
 
 
-        },
-        success: function (data) {
-			
-		    pic = data;
-			console.log("picture received");
-        },
-        error: function (request, error) {
-
-            //Log failure
-            var myError = "Error " + request.status + ": " + request.responseJSON.Message;
-            navigator.notification.alert(myError, console.log(myError), "Get Status Failed");
-        }
-
-    });
-	
-	return pic;
-}
+    }
 
 });
 
