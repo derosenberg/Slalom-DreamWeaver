af31cadc4795e76d8008c293f8f3c5adcad73d39
gkgay <gkgay@crimson.ua.edu>
2015-04-08 19:38:52 -0500

    change password start

---
+++ www/js/changePassword.js
@@ -0,0 +1,18 @@
+$(document).on('pageinit', '#regisCamera', function () {
+	
+	$(document).on('click', '#changePasswordBttn', function () {
+		
+		   var currentPword = $("#currentPassword").val();
+		   var newPword = $("#newPassword").val();
+		   var confirmNewPword = $("#confirmNewPassword").val();
+		
+		if (newPword = confirmNewPword){
+			
+			
+			
+		}
+		
+	});
+	
+	
+});
