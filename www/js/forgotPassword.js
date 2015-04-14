f1f188b4c3e7830f1824c47b8ae15e907edea988
ezeitouni <cezeitouni@crimson.ua.edu>
2015-04-14 17:44:24 -0500

    forgot email fix

--- www/js/forgotPassword.js
+++ www/js/forgotPassword.js
@@ -2,35 +2,27 @@
 
 //Forgot Password Page Controls
 $(document).on('pageinit', '#forgotPassword', function () {
-
+    $(document).on('click', '#forgotPasswordBtn', function () {
     //AJAX call for password recovery
-    ResetPassword($('#emailForgot').val());
-    $('#emailForgot').val("");
+        ResetPassword($('#emailForgot').val());
+    });
 });
 //------------------------------------------------------------------------------------------------------------
 
 //AJAX Call to reset the password
 //On success or failure there is an alert and redirect to the login page
 function ResetPassword(email) {
-    $(document).on('click', '#forgotPasswordBtn', function () {
+    
         $.ajax({
             url: S_ROOT + 'api/Account/forgotpassword/',
             data: "Email=" + email,
             type: 'POST',
             async: true,
             contentType: "application/x-www-form-urlencoded",
-            beforeSend: function () {
-
-                //Shows the page loading message
-                $.mobile.showPageLoadingMsg(true);
-            },
             complete: function () {
-
-                //Hides the loading message
-                $.mobile.hidePageLoadingMsg();
+                $('#emailForgot').val("");
             },
             success: function () {
-
                 //Notifies the user that the password was reset and returns the user to the login page
                 navigator.notification.alert("Your new password has been delivered to you by email.", $.mobile.changePage("#login"), "Password Reset");
 
@@ -39,9 +31,9 @@ function ResetPassword(email) {
 
                 //Alerts the user that the request failed and then changes the page to login
                 var authError = request.status + ": " + request.responseJSON.Message;
-                navigator.notification.alert(authError, console.log(authError), "Login Failed");
+                navigator.notification.alert(authError, console.log(authError), "Error Resetting Password");
                 $.mobile.changePage("#login");
             }
         });
-    });
+    
 }
